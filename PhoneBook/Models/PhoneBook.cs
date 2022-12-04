f3690a7d1ecb52737e4a35a8e78113c945e5707c
Bai Jaitrong <bjaitrong@comcast.net>
2022-12-04 11:33:10 -0700

    Add AddContact() method and Dictionary Object.

---
+++ PhoneBook/Models/PhoneBook.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+
+namespace PhoneBook
+{
+  class PhoneBook
+  {
+    public static Dictionary<string,string> phoneNumbers = new Dictionary<string,string>();
+
+    static void AddContact()
+    {
+      Console.WriteLine("New Contact");
+      Console.WriteLine("Enter a New Contact name");
+      string name = Console.ReadLine();
+      if(phoneNumbers.ContainsKey(name))
+      {
+        Console.WriteLine("That person is already in your phonebook. their number is " + phoneNumbers[name]);
+      }
+      else
+      {
+        phoneNumbers.Add(name, number);
+      }
+    }
+  }
+
+}
+
